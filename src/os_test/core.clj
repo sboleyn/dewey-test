fe58bad7d72ab98b54b01d6cdf210a28ab4a23af
sboleyn <sboleyn@arizona.edu>
2023-08-31 14:45:58 -0700

    Change variable c to es to match the rest of the files

--- src/os_test/core.clj
+++ src/os_test/core.clj
@@ -15,7 +15,8 @@
    [me.raynes.fs :as fs]
    [service-logging.thread-context :as tc]
    [qbits.spandex :as s])
-  (:import [java.net URL]))
+  (:import [java.net URL]
+           [java.util Properties]))
 
 (defn- init-es
   "Establishes a connection to elasticsearch"
@@ -48,3 +49,94 @@
               (cfg/irods-home)
               (cfg/irods-zone)
               (cfg/irods-default-resource)))
+
+(defn- connect-to-broker?
+  [irods-cfg c]
+  (try
+    (amq/attach-to-exchange (cfg/amqp-uri)
+                            (cfg/amqp-queue-name)
+                            (cfg/amqp-exchange)
+                            (cfg/amqp-exchange-durable)
+                            (cfg/amqp-exchange-autodelete)
+                            (cfg/amqp-qos)
+                            (partial curation/consume-msg irods-cfg c)
+                            "data-object.#"
+                            "collection.#")
+    (log/info (format "Attached to the AMQP broker. uri=%s exchange=%s queue=%s" (cfg/amqp-uri) (cfg/amqp-exchange) (cfg/amqp-queue-name)))
+    true
+    (catch Exception e
+      (log/info e "Failed to attach to the AMQP broker. Retrying...")
+      false)))
+
+(defn- connect-to-events-broker?
+  []
+  (try
+    (let [queue-name (str "events.dewey.queue." (cfg/environment-name))]
+      (amq/attach-to-exchange (cfg/amqp-events-uri)
+                              queue-name
+                              (cfg/amqp-events-exchange)
+                              (cfg/amqp-events-exchange-durable?)
+                              (cfg/amqp-events-exchange-auto-delete?)
+                              (cfg/amqp-qos)
+                              events/consume-msg
+                              "events.dewey.#")
+      (log/info "Attached to the events AMQP broker on queue" queue-name)
+      true)
+    (catch Exception e
+      (log/info e "Failed to attach to the evemts AMQP broker. Retrying...")
+      false)))
+
+(defn- listen
+  [irods-cfg c]
+  (let [attached? (connect-to-broker? irods-cfg c)]
+    (when-not attached?
+      (Thread/sleep 1000)
+      (recur irods-cfg c))))
+
+(defn- listen-for-events
+  []
+  (when-not (connect-to-events-broker?)
+    (Thread/sleep 1000)
+    (recur)))
+
+(defn- listen-for-status
+  []
+  (.start
+   (Thread.
+    (partial status/start-jetty (cfg/listen-port)))))
+
+(defn- run
+  []
+  (listen-for-status)
+  (listen (init-irods) (init-es))
+  (listen-for-events))
+
+(def svc-info
+  {:desc "Service that keeps an elasticsearch index synchronized with an iRODS repository."
+   :app-name "dewey"
+   :group-id "org.cyverse"
+   :art-id "dewey"
+   :service "dewey"})
+
+
+(defn cli-options
+  []
+  [["-c" "--config PATH" "Path to the config file"
+    :default "/etc/iplant/de/dewey.properties"]
+   ["-v" "--version" "Print out the version number."]
+   ["-h" "--help"]])
+
+
+(defn -main
+  [& args]
+  (tc/with-logging-context svc-info
+    (try+
+     (let [{:keys [options arguments errors summary]} (ccli/handle-args svc-info args cli-options)]
+       (when-not (fs/exists? (:config options))
+         (ccli/exit 1 "The config file does not exist."))
+       (when-not (fs/readable? (:config options))
+         (ccli/exit 1 "The config file is not readable."))
+       (cfg/load-config-from-file (:config options))
+       (run))
+     (catch Object _
+       (log/error (:throwable &throw-context) "UNEXPECTED ERROR - EXITING")))))
--- src/os_test/indexing.clj
+++ src/os_test/indexing.clj
@@ -6,91 +6,41 @@
             [clojure-commons.file-utils :as file]
             [os-test.doc-prep :as prep]))
 
-(def docu {:id "0209c39e-37ef-11ec-8a85-28924acd7818"
-           :doc_type "file"
-           :path "/cyverse/home/tswetnam/ept/ept-data/8-77-7-132.laz"
-           :label "8-77-7-132.laz"
-           :creator "de-irods#cyverse"
-           :dateCreated 1656707727000
-           :dateModified 1656707727000
-           :fileSize 205090
-           :fileType "generic"
-           :userPermissions [{:user "de-irods#cyverse"
-                              :permission "own"}
-                             {:user "rodsadmin#cyverse"
-                              :permission "own"}]
-           :metadata {:irods [{:attribute "ipc-filetype"
-                               :value "unknown"
-
-                               :unit "ipc-info-typer"}]}})
-
 (defn- index-doc
-  [c doc]
-  (s/request c {:url
-                [(cfg/es-index) :_doc (str (:id doc))]
-                :method :put
-                :headers {"Content-Type" "application/json"}
-                :body doc}))
+  [es doc]
+  (s/request es {:url
+                 [(cfg/es-index) :_doc (str (:id doc))]
+                 :method :put
+                 :headers {"Content-Type" "application/json"}
+                 :body doc}))
 
 (defn- update-doc
   "Scripted updates which are only compatible with Elasticsearch 5.x and greater."
-  [c entity script params]
-  (s/request c {:url [(cfg/es-index) :_update (str (entity/id entity))]
-                :method :post
-                :headers {"Content-Type" "application/json"}
-                ;;:body {:script {:inline script :lang "painless" :params params}}}))
-                :body {"script" {"source" script "lang" "painless" "params" params}}}))
+  [es entity script params]
+  (s/request es {:url [(cfg/es-index) :_update (str (entity/id entity))]
+                 :method :post
+                 :headers {"Content-Type" "application/json"}
+                 :body {"script" {"source" script "lang" "painless" "params" params}}}))
 
-;; (defn entity-indexed?
-;;   ;; DOC FIX
-;;   ;; Fix ability to pass in id
-;;   ;; (ns were-creatures)
-;;   ;; ➊ (defmulti full-moon-behavior (fn [were-creature] (:were-type were-creature)))
-;;   ;; ➋ (defmethod full-moon-behavior :wolf
-;;   ;;     [were-creature]
-;;   ;;     (str (:name were-creature) " will howl and murder"))
-;;   ;; ➌ (defmethod full-moon-behavior :simmons
-;;   ;;     [were-creature]
-;;   ;;     (str (:name were-creature) " will encourage people and sweat to the oldies"))
-
-
-;;   ;; (full-moon-behavior {:were-type :wolf
-;;   ;; ➍                      :name "Rachel from next door"})
-;;      ; => "Rachel from next door will howl and murder"
-;;   [c entity]
-;;   ^{:doc "Determines whether or not an iRODS entity has been indexed.
-
-;;              Parameters:
-;;                c     - the elasticsearch connection
-;;                entity - the entity being checked
-
-;;              Throws:
-;;                This function can throw an exception if FIX"}
-;;   (try
-;;     (s/request c {:url [(cfg/es-index) :_doc (str (entity/id entity))]
-;;     ;;(s/request c {:url [(cfg/es-index) :_doc "0f14883e-37fa-11ec-8a85-28924acd781foo"]
-;;                   :method :head}) true
-;;     (catch Exception e (println (format "Error %s" e)) false)))
-
-(defn indexing-method
-  [entity]
+(defn entity-id-switch
+  [entity-val]
   (cond
-    (string? entity) :string
-    (map? entity) :map
+    (string? entity-val) :string
+    (map? entity-val) :map
     :else :unknown))
 
-(defmulti entity-indexed? (fn [c entity] (indexing-method entity)))
+(defmulti entity-indexed? (fn [_c entity-val] (entity-id-switch entity-val)))
 (defmethod entity-indexed? :string
-  [c entity-id]
+  [es entity-id]
   (try
-    (s/request c {:url [(cfg/es-index) :_doc entity-id]
-                  :method :head}) true
+    (s/request es {:url [(cfg/es-index) :_doc entity-id]
+                   :method :head}) true
     (catch Exception e (println (format "Error %s" e)) false)))
 (defmethod entity-indexed? :map
-  [c entity]
+  [es entity]
   (try
-    (s/request c {:url [(cfg/es-index) :_doc (str (entity/id entity))]
-                  :method :head}) true
+    (s/request es {:url [(cfg/es-index) :_doc (str (entity/id entity))]
+                   :method :head}) true
     (catch Exception e (println (format "Error %s" e)) false)))
 
 
@@ -98,13 +48,13 @@
   "Indexes a collection.
 
    Parameters:
-     c    - the elasticsearch connection
+     es    - the elasticsearch connection
      coll  - the collection entity to index
 
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. The
      function can also throw one if the collection is not in the iRODS data store."
-  [c coll]
+  [es coll]
   (let [folder (prep/format-folder (entity/id coll)
                                    (entity/path coll)
                                    (entity/acl coll)
@@ -112,13 +62,13 @@
                                    (entity/creation-time coll)
                                    (entity/modification-time coll)
                                    (entity/metadata coll))]
-    (index-doc c folder)))
+    (index-doc es folder)))
 
 (defn index-data-object
   "Indexes a data object.
 
    Parameters:
-     c        - the elasticsearch connection
+     es        - the elasticsearch connection
      obj       - The CollectionAndDataObjectListingEntry of the data object to index
      creator   - (Optional) The username of the creator of the data object
      file-size - (Optional) The byte size of the data object
@@ -127,7 +77,7 @@
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. The
      function can also throw one if the data object is not in the iRODS data store."
-  [c obj & {:keys [creator file-size file-type]}]
+  [es obj & {:keys [creator file-size file-type]}]
   (let [file (prep/format-file (entity/id obj)
                                (entity/path obj)
                                (entity/acl obj)
@@ -137,7 +87,7 @@
                                (entity/metadata obj)
                                (or file-size (entity/size obj))
                                (or file-type (entity/media-type obj)))]
-    (index-doc c file)))
+    (index-doc es file)))
 
 
 ;; (s/request es {:url ["data_test1" :_doc "0f0f3be6-377c-11ec-8a85-28924acd7818"] :method :delete})
@@ -146,15 +96,15 @@
   "Removes an iRODS entity from the search index.
 
    Parameters:
-     c          - the elasticsearch connection
+     es          - the elasticsearch connection
      entity-type - :collection|:data-object
      entity-id   - The UUID of the entity to remove
 
    Throws:
      This function can throw an exception if it can't connect to elasticsearch."
-  [c entity-id]
-  (when (entity-indexed? c (str entity-id))
-    (s/request c {:url [(cfg/es-index) :_doc (str entity-id)]})))
+  [es entity-id]
+  (when (entity-indexed? es (str entity-id))
+    (s/request es {:url [(cfg/es-index) :_doc (str entity-id)]})))
 
 (defn remove-entities-like
   "Removes iRODS entities from the search index that have a path matching the provide glob. The glob
@@ -164,7 +114,7 @@
   Elasticsearch versions prior to 5.x.
 
    Parameters:
-     c        - the elasticsearch connection
+     es        - the elasticsearch connection
      path-glob - the glob describing the paths of the entities to remove
 
    Throws:
@@ -179,17 +129,17 @@
 ;;       HttpAsyncResponseConsumerFactory/DEFAULT}
 ;;  :as request-params})
 
-  [c path-glob]
+  [es path-glob]
   ;; (rest/post es
   ;;            (rest/url-with-path es (cfg/es-index) "_delete_by_query")
   ;;            {:body {:query (es-query/wildcard :path path-glob)}}))
 
   ;; POST <index>/_delete_by_query
 
-  (s/request c {:url [(cfg/es-index) :_delete_by_query]
-                :query-string "analyze_wildcard=true"
-                :method :post
-                :body {:query {:wildcard {:path path-glob}}}}))
+  (s/request es {:url [(cfg/es-index) :_delete_by_query]
+                 :query-string "analyze_wildcard=true"
+                 :method :post
+                 :body {:query {:wildcard {:path path-glob}}}}))
 
 ; XXX - I wish I could think of a way to cleanly and simply separate out the document update logic
 ; from the update scripts calls in the following functions. It really belongs with the rest of the
@@ -199,20 +149,20 @@
   "Updates the path of an entity and optionally its modification time
 
    Parameters:
-     c       - the elasticsearch connection
+     es       - the elasticsearch connection
      entity   - the entity to update
      path     - the entity's new path
      mod-time - (Optional) the entity's modification time"
-  ([c entity path]
-   (update-doc c
+  ([es entity path]
+   (update-doc es
                entity
                "ctx._source.path = params.path;
                 ctx._source.label = params.label;"
                {:path  path
                 :label (file/basename path)}))
 
-  ([c entity path mod-time]
-   (update-doc c
+  ([es entity path mod-time]
+   (update-doc es
                entity
                "ctx._source.path = params.path;
                  ctx._source.label = params.label;
@@ -225,14 +175,14 @@
   "Updates the indexed ACL of an entity.
 
    Parameters:
-     c     - the elasticsearch connection
+     es     - the elasticsearch connection
      entity - the entity whose ACL needs to be updated in elasticsearch
 
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. It can also
      throw if the entity has no index entry or is not in the iRODS data store."
-  [c entity]
-  (update-doc c
+  [es entity]
+  (update-doc es
               entity
               "ctx._source.userPermissions = params.permissions"
               {:permissions (prep/format-acl (entity/acl entity))}))
@@ -241,14 +191,14 @@
   "Updates the indexed AVU metadata of an entity.
 
    Parameters:
-     c     - the elasticsearch connection
+     es     - the elasticsearch connection
      entity - The entity whose metadata needs to be updated in elasticsearch
 
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. It can also
      throw if the entity has no index entry or is not in the iRODS data store."
-  [c entity]
-  (update-doc c
+  [es entity]
+  (update-doc es
               entity
               "ctx._source.metadata = params.metadata"
               {:metadata (prep/format-metadata (entity/metadata entity))}))
@@ -257,14 +207,14 @@
   "Updates the indexed modify time of a collection.
 
    Parameters:
-     c   - the elasticsearch connection
+     es   - the elasticsearch connection
      coll - the collection that was modified
 
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. It can
      also throw if the collection has no index entry or is not in the iRODS data store."
-  [c coll]
-  (update-doc c
+  [es coll]
+  (update-doc es
               coll
               "if (params.dateModified > ctx._source.dateModified) { ctx._source.dateModified = params.dateModified } else { ctx.op = \"none\" }"
               {:dateModified (prep/format-time (entity/modification-time coll))}))
@@ -274,7 +224,7 @@
    file type for the data object.
 
    Parameters:
-     c        - the elasticsearch connection
+     es        - the elasticsearch connection
      obj       - the data object that was modified
      file-size - The data object's file size in bytes.
      file-type - (OPTIONAL) The media type of the data object.
@@ -282,16 +232,16 @@
    Throws:
      This function can throw an exception if it can't connect to elasticsearch or iRODS. It can
      also throw if the data object has no index entry or is not in the iRODS data store."
-  ([c obj file-size]
-   (update-doc c
+  ([es obj file-size]
+   (update-doc es
                obj
                "if (params.dateModified > ctx._source.dateModified) { ctx._source.dateModified = params.dateModified; }
                 ctx._source.fileSize = params.fileSize;"
                {:dateModified (prep/format-time (entity/modification-time obj))
                 :fileSize     file-size}))
 
-  ([c obj file-size file-type]
-   (update-doc c
+  ([es obj file-size file-type]
+   (update-doc es
                obj
                "if (params.dateModified > ctx._source.dateModified) { ctx._source.dateModified = params.dateModified; }
                 ctx._source.fileSize = params.fileSize;
